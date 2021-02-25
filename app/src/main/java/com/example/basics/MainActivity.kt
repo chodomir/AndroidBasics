af58f431fb7a0fc9b08aa79daffe6d904e30666f
Danilo <danilo.devedzic@outlook.com>
2021-02-25 14:30:54 +0100

    Refactored button names, added a new button

--- app/src/main/java/com/example/basics/MainActivity.kt
+++ app/src/main/java/com/example/basics/MainActivity.kt
@@ -8,6 +8,7 @@ import android.util.Log
 import android.widget.Button
 import java.util.function.Consumer
 
+// MainActivity launchMode set to "standard"
 class MainActivity : AppCompatActivity() {
     companion object {
         val TAG = "MainActivity"
@@ -17,9 +18,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btn: Button = findViewById(R.id.btnToSecondary)
-        btn.setOnClickListener {
+        val btnToSecondary: Button = findViewById(R.id.btnMainSecondary)
+        val btnToMain: Button = findViewById(R.id.btnMainMain)
+
+        // Set button click listeners
+        btnToSecondary.setOnClickListener {
             val intent = Intent(this, SecondaryActivity::class.java)
+            startActivity(intent)
+        }
+        btnToMain.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
 
