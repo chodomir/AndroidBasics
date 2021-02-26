21d6e3b2e137e7ea8181d221c8bb9506055c8580
Danilo <danilo.devedzic@outlook.com>
2021-02-26 12:51:04 +0100

    Added a button that starts ResultActivity for result in MainActivity

--- app/src/main/java/com/example/basics/MainActivity.kt
+++ app/src/main/java/com/example/basics/MainActivity.kt
@@ -6,22 +6,30 @@ import android.os.Bundle
 import android.os.CancellationSignal
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import java.util.function.Consumer
 
 // MainActivity launchMode set to "standard"
 class MainActivity : AppCompatActivity() {
     companion object {
         val TAG = "MainActivity"
+        val LAUNCH_RESULT_ACITIVITY = 0
     }
+
+    lateinit var tvActivityResult: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // get text view that displays return value of started activity
+        tvActivityResult = findViewById(R.id.tvActivityResult)
+
         val btnToSecondary: Button = findViewById(R.id.btnMainSecondary)
         val btnToMain: Button = findViewById(R.id.btnMainMain)
         val btnToTertiary: Button = findViewById(R.id.btnMainTertiary)
         val btnToQuaternary: Button = findViewById(R.id.btnMainQuaternary)
+        val btnToResultActivity: Button = findViewById(R.id.btnStartActivityForResult)
 
         // Set button click listeners
         btnToSecondary.setOnClickListener {
@@ -40,8 +48,24 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, QuaternaryActivity::class.java)
             startActivity(intent)
         }
+        btnToResultActivity.setOnClickListener {
+            val intent = Intent(this, ResultActivity::class.java).apply {
+                putExtra("data", "Hello World!")
+            }
+            startActivityForResult(intent, LAUNCH_RESULT_ACITIVITY)
+        }
 
         Log.d(TAG, "onCreate() method called.")
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        Log.d(TAG, "onActivityResult() method called.")
+        super.onActivityResult(requestCode, resultCode, data)
+
+        when (requestCode) {
+            LAUNCH_RESULT_ACITIVITY ->
+                tvActivityResult.text = data?.getStringExtra("result")
+        }
     }
 
     override fun onStart() {
