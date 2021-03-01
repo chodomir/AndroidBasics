c68ad77c08b645951165054f1f118e9e57e90dca
Danilo <danilo.devedzic@outlook.com>
2021-03-01 12:36:01 +0100

    Added basic functionality for UIFragment

--- app/src/main/java/com/example/basics/UIFragment.kt
+++ app/src/main/java/com/example/basics/UIFragment.kt
@@ -1,59 +1,77 @@
 package com.example.basics
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CheckBox
+import kotlin.reflect.KClass
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [UIFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class UIFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var cbFlagNewTask: CheckBox
+    private lateinit var cbFlagClearTop: CheckBox
+    private lateinit var cbFlagSingleTop: CheckBox
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ui, container, false)
+        val view = inflater.inflate(R.layout.fragment_ui, container, false)
+
+        // get buttons
+        val btnStandard: Button = view.findViewById(R.id.btnStandard)
+        val btnSingleTop: Button = view.findViewById(R.id.btnSingleTop)
+        val btnSingleTask: Button = view.findViewById(R.id.btnSingleTask)
+        val btnSingleInstance: Button = view.findViewById(R.id.btnSingleInstance)
+
+        // get checkboxes
+        cbFlagNewTask = view.findViewById(R.id.cbFlagNewTask)
+        cbFlagClearTop = view.findViewById(R.id.cbFlagClearTop)
+        cbFlagSingleTop = view.findViewById(R.id.cbFlagSingleTop)
+
+        // set button click listeners
+        btnStandard.setOnClickListener{
+            val intent = createIntentForStartActivity(MainActivity::class)
+            startActivity(intent)
+        }
+        btnSingleTop.setOnClickListener {
+            val intent = createIntentForStartActivity(SecondaryActivity::class)
+            startActivity(intent)
+        }
+        btnSingleTask.setOnClickListener {
+            val intent = createIntentForStartActivity(TertiaryActivity::class)
+            startActivity(intent)
+        }
+        btnSingleInstance.setOnClickListener {
+            val intent = createIntentForStartActivity(QuaternaryActivity::class)
+            startActivity(intent)
+        }
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment UIFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            UIFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun createFlag(newTaskChecked: Boolean, clearTopChecked: Boolean,
+                           singleTopChecked: Boolean): Int {
+        var flag = if (newTaskChecked) Intent.FLAG_ACTIVITY_NEW_TASK else 0
+        flag = if (clearTopChecked) flag or Intent.FLAG_ACTIVITY_CLEAR_TOP else flag
+        flag = if (singleTopChecked) flag or Intent.FLAG_ACTIVITY_SINGLE_TOP else flag
+
+        return flag
+    }
+
+    private fun createIntentForStartActivity(cls: KClass<*>): Intent {
+        return Intent(activity, cls.java).apply {
+            val flag = createFlag(
+                    cbFlagNewTask.isChecked,
+                    cbFlagClearTop.isChecked,
+                    cbFlagSingleTop.isChecked
+            )
+            if (flag != 0) addFlags(flag)
+        }
     }
 }
