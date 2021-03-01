1deb19c7ff322dc2171f1a47461c5296d280a38b
Danilo <danilo.devedzic@outlook.com>
2021-03-01 13:18:02 +0100

    Logging most important fragment lifecycle callback methods

--- app/src/main/java/com/example/basics/UIFragment.kt
+++ app/src/main/java/com/example/basics/UIFragment.kt
@@ -1,7 +1,9 @@
 package com.example.basics
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -11,15 +13,30 @@ import android.widget.CheckBox
 import kotlin.reflect.KClass
 
 class UIFragment : Fragment() {
+    companion object {
+        val TAG = "UIFragment"
+    }
 
     private lateinit var cbFlagNewTask: CheckBox
     private lateinit var cbFlagClearTop: CheckBox
     private lateinit var cbFlagSingleTop: CheckBox
 
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        Log.d(TAG, "onAttach() method called")
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate() method called")
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        Log.d(TAG, "onCreateView() method called")
+
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_ui, container, false)
 
@@ -53,6 +70,56 @@ class UIFragment : Fragment() {
         }
 
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d(TAG, "onViewCreated() method called.")
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+        Log.d(TAG, "onViewStateRestored() method called")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() method called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() method called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() method called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() method called")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d(TAG, "onSaveInstanceState() method called")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        Log.d(TAG, "onDestroyView() method called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() method called")
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        Log.d(TAG, "onDetach() method called")
     }
 
     private fun createFlag(newTaskChecked: Boolean, clearTopChecked: Boolean,
