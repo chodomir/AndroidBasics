2e66f76bed23e47f53bb6354b4b17b1c4f536313
Danilo <danilo.devedzic@outlook.com>
2021-03-02 13:32:25 +0100

    Removed unused import in UIFragment

--- app/src/main/java/com/example/basics/UIFragment.kt
+++ app/src/main/java/com/example/basics/UIFragment.kt
@@ -2,7 +2,6 @@ package com.example.basics
 
 import android.content.Context
 import android.content.Intent
-import android.opengl.Visibility
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
