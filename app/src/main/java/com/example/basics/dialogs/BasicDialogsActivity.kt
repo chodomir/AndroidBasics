a56786107b721bad70a8e2519018f6122fc6dcd5
Danilo <danilo.devedzic@outlook.com>
2021-03-03 11:36:46 +0100

    BottomSheetBehavior implemented in BasicDialogsActivity

--- app/src/main/java/com/example/basics/dialogs/BasicDialogsActivity.kt
+++ app/src/main/java/com/example/basics/dialogs/BasicDialogsActivity.kt
@@ -5,9 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.DialogFragment
 import com.example.basics.R
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.snackbar.Snackbar
 
 class BasicDialogsActivity : AppCompatActivity(),
@@ -42,13 +44,18 @@ class BasicDialogsActivity : AppCompatActivity(),
 
             return@setOnLongClickListener true
         }
+
+        // Bottomsheet behavior configuration
+        val bottomSheetBehavior = BottomSheetBehavior.from(findViewById<LinearLayout>(R.id.linearLayout))
+        bottomSheetBehavior.peekHeight = 400
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED;
     }
 
     override fun onStart() {
         super.onStart()
 
         val basicdf = BasicDialogFragment()
-        basicdf.show(supportFragmentManager, "BasicDialog FragmentManager")
+        basicdf.show(supportFragmentManager, BasicDialogFragment.TAG)
     }
 
     override fun onDialogPositiveClick(dialog: DialogFragment) {
