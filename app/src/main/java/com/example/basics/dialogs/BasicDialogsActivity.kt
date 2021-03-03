92b0ed46b0bc771df1037b72356b5fdc81942c99
Danilo <danilo.devedzic@outlook.com>
2021-03-03 13:46:54 +0100

    Modal Navigation Drawer implemented

--- app/src/main/java/com/example/basics/dialogs/BasicDialogsActivity.kt
+++ app/src/main/java/com/example/basics/dialogs/BasicDialogsActivity.kt
@@ -4,12 +4,17 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Gravity
 import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.DialogFragment
 import com.example.basics.R
+import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
 
 class BasicDialogsActivity : AppCompatActivity(),
@@ -27,6 +32,20 @@ class BasicDialogsActivity : AppCompatActivity(),
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_basic_dialogs)
+
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
+        val topAppBar = findViewById<MaterialToolbar>(R.id.topAppBar)
+        val navigationView = findViewById<NavigationView>(R.id.navigationView)
+        topAppBar.setNavigationOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
+
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            // Handle menu item selected
+            menuItem.isChecked = true
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
+        }
 
         val tvColoredText: TextView = findViewById(R.id.tvColoredText)
         tvColoredText.text = "Hold to change color"
