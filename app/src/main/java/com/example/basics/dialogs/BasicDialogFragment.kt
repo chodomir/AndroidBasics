05762fc3c8a294ade3d8254b253c7f2db98359b2
Danilo <danilo.devedzic@outlook.com>
2021-03-03 10:44:57 +0100

    BasicDialogFragment sends event listeners to host activity which implements BasicDialogFragment.BasicDialogListener interface

--- app/src/main/java/com/example/basics/dialogs/BasicDialogFragment.kt
+++ app/src/main/java/com/example/basics/dialogs/BasicDialogFragment.kt
@@ -2,6 +2,7 @@ package com.example.basics.dialogs
 
 import android.app.AlertDialog
 import android.app.Dialog
+import android.content.Context
 import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
@@ -10,8 +11,28 @@ import com.example.basics.R
 import java.lang.IllegalStateException
 
 class BasicDialogFragment : DialogFragment() {
+    internal lateinit var listener: BasicDialogListener
+
+    interface BasicDialogListener {
+        fun onDialogPositiveClick(dialog: DialogFragment)
+        fun onDialogNegativeClick(dialog: DialogFragment)
+    }
+
     companion object {
         val TAG: String = "BasicDialogFragment"
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        // Verify that the host activity implements the callback interface
+        try {
+            // Instantiate the NoticeDialogListener so we can send events to the host
+            listener = context as BasicDialogListener
+        } catch (e: ClassCastException) {
+            // The activity doesn't implement the interface, throw exception
+            throw ClassCastException((context.toString() +
+                    " must implement BasicDialogListener"))
+        }
     }
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
@@ -21,10 +42,14 @@ class BasicDialogFragment : DialogFragment() {
                     .setPositiveButton(R.string.done,
                     DialogInterface.OnClickListener { _, id ->
                         Log.d(TAG, "Positive button pressed, id: $id")
+                        // Send the positive button event back to the host activity
+                        listener.onDialogPositiveClick(this)
                     })
                     .setNegativeButton(R.string.cancel,
                     DialogInterface.OnClickListener { _, id ->
                         Log.d(TAG, "Negative button pressed, id: $id")
+                        // Send the positive button event back to the host activity
+                        listener.onDialogNegativeClick(this)
                     })
             builder.create()
         } ?: throw IllegalStateException("Activity cannot be null")
--- app/src/main/java/com/example/basics/dialogs/BasicDialogsExample.kt
+++ app/src/main/java/com/example/basics/dialogs/BasicDialogsExample.kt
@@ -6,10 +6,15 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.TextView
+import androidx.fragment.app.DialogFragment
 import com.example.basics.R
 import com.google.android.material.snackbar.Snackbar
 
-class BasicDialogsExample : AppCompatActivity() {
+class BasicDialogsExample : AppCompatActivity(),
+        BasicDialogFragment.BasicDialogListener {
+    companion object {
+        val TAG: String = "BasicDialogsExample"
+    }
 
     class MyUndoListener(var tvText: TextView) : View.OnClickListener {
         override fun onClick(v: View?) {
@@ -44,5 +49,13 @@ class BasicDialogsExample : AppCompatActivity() {
 
         val basicdf = BasicDialogFragment()
         basicdf.show(supportFragmentManager, "BasicDialog FragmentManager")
+    }
+
+    override fun onDialogPositiveClick(dialog: DialogFragment) {
+        Log.d(TAG, "Positive button clicked (host activity)")
+    }
+
+    override fun onDialogNegativeClick(dialog: DialogFragment) {
+        Log.d(TAG, "Negative button clicked (host activity)")
     }
 }
