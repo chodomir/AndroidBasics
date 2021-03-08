3506794214ba477ad9496e90140d8e5987adf340
Danilo <danilo.devedzic@outlook.com>
2021-03-08 10:43:47 +0100

    Added radio buttons to switch between different context menus

--- app/src/main/java/com/example/basics/d10CustomDialog/MyDialog.kt
+++ app/src/main/java/com/example/basics/d10CustomDialog/MyDialog.kt
@@ -4,13 +4,18 @@ import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import com.example.basics.R
 
 class MyDialog(context: Context) : Dialog(context, R.style.D10MyDialog) {
     companion object {
         val TAG = "MyDialog"
     }
+
+    var floatingContextMenu: Boolean = true
 
     lateinit var btnPositive: Button
     lateinit var btnNegative: Button
@@ -23,8 +28,13 @@ class MyDialog(context: Context) : Dialog(context, R.style.D10MyDialog) {
     }
 
     private fun setupButtons() {
+        val radioButton1: RadioButton = findViewById(R.id.d10_radio_button_1)
+        val radioButton2: RadioButton = findViewById(R.id.d10_radio_button_2)
         btnPositive = findViewById(R.id.d10_button_positive)
         btnNegative = findViewById(R.id.d10_button_negative)
+
+        radioButton1.setOnClickListener { onRadioButtonClicked(it) }
+        radioButton2.setOnClickListener { onRadioButtonClicked(it) }
 
         btnPositive.setOnClickListener {
             Log.d(TAG, "Positive button clicked")
@@ -32,8 +42,19 @@ class MyDialog(context: Context) : Dialog(context, R.style.D10MyDialog) {
         }
         btnNegative.setOnClickListener {
             Log.d(TAG, "Negative button clicked")
-            dismiss()
         }
+    }
+
+    private fun onRadioButtonClicked(view: View) {
+        if (view is RadioButton) {
+            val checked = view.isChecked
+            floatingContextMenu = (view.id == R.id.d10_radio_button_1 && checked)
+            Log.d(TAG,"Radio button clicked: floating context menu = $floatingContextMenu")
+        }
+    }
+
+    fun shouldShowFloatingContextMenu() : Boolean {
+        return floatingContextMenu
     }
 
 
