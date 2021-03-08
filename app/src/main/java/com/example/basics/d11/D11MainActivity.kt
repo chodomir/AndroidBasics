4025cb698741856b0ffca176c28afd521d243293
Danilo <danilo.devedzic@outlook.com>
2021-03-08 15:00:37 +0100

    D11MainActivity sends a broadcast onStart()

--- app/src/main/java/com/example/basics/d11/D11MainActivity.kt
+++ app/src/main/java/com/example/basics/d11/D11MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.basics.R
 
+private const val CUSTOM_ACTION = "action.skuza.skuza.videh.te"
 private val receiver = MyBroadcastReceiver()
 
 class D11MainActivity : AppCompatActivity() {
@@ -13,8 +14,17 @@ class D11MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_d11_main)
 
-        val intentFilter = IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+        val intentFilter = IntentFilter(CUSTOM_ACTION)
         registerReceiver(receiver, intentFilter)
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Intent().also { intent ->
+            intent.action = CUSTOM_ACTION
+            sendBroadcast(intent)
+        }
     }
 
     override fun onDestroy() {
