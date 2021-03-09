2301c961a09e2ca4d235ca04429b6399c03ae43c
Danilo <danilo.devedzic@outlook.com>
2021-03-09 17:34:58 +0100

    Implemented runtime permission request

--- app/src/main/java/com/example/basics/d12/D12MainActivity.kt
+++ app/src/main/java/com/example/basics/d12/D12MainActivity.kt
@@ -1,11 +1,68 @@
 package com.example.basics.d12
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.core.content.ContextCompat
+import com.example.basics.R
+
+private const val TAG = "D12MainActivity"
+private const val CAMERA_REQUEST_CODE = 1
 
 class D12MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_d12_main)
+
+        when {
+            ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.CAMERA
+                    ) == PackageManager.PERMISSION_GRANTED -> {
+                Log.d(TAG, "Using feature Y")
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.CAMERA) -> {
+                Log.d(TAG, "Showing the rationale...bla bla bla")
+            }
+            else -> {
+                requestPermissions(arrayOf(Manifest.permission.CAMERA), CAMERA_REQUEST_CODE)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode) {
+            CAMERA_REQUEST_CODE -> {
+                if ((grantResults.isNotEmpty() &&
+                            grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    // Permission is granted. Continue the action or workflow
+                    // in your app.
+                    Log.d(TAG, "Permission is granted")
+                } else {
+                    // Explain to the user that the feature is unavailable because
+                    // the features requires a permission that the user has denied.
+                    // At the same time, respect the user's decision. Don't link to
+                    // system settings in an effort to convince the user to change
+                    // their decision.
+                    Log.d(TAG, "Permission denied! :(")
+                }
+                return
+            }
+
+            else -> {
+                // Ignore all other requests.
+            }
+        }
     }
 }
