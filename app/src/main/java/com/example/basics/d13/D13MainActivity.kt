b64336a48f5c448623adae9401fa4436ac1d8da2
Danilo <danilo.devedzic@outlook.com>
2021-03-10 14:35:12 +0100

    Added D13MainActivity - main activity for task given at day 13

---
+++ app/src/main/java/com/example/basics/d13/D13MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.basics.d13
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class D13MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_d13_main)
+    }
+}
