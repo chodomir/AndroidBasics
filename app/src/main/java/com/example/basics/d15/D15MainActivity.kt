bcc332ef3b8e94742cb4256bdff19a240bda6c79
Danilo <danilo.devedzic@outlook.com>
2021-03-12 14:27:39 +0100

    Added D15MainActivity, entry point for threadings examples

---
+++ app/src/main/java/com/example/basics/d15/D15MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.basics.d15
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class D15MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_d15_main)
+    }
+}
