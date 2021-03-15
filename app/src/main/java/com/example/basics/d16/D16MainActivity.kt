4720e324d18dc53334b13a4d65c882a7a60a250c
Danilo <danilo.devedzic@outlook.com>
2021-03-15 17:34:57 +0100

    Added D16MainActivity - simple theme and style example

---
+++ app/src/main/java/com/example/basics/d16/D16MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.basics.d16
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class D16MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_d16_main)
+    }
+}
