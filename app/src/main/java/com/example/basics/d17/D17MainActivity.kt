84fd8437e76b5f788a95696767a67c09b3687954
Danilo <danilo.devedzic@outlook.com>
2021-03-16 11:43:46 +0100

    Added D17MainActivity activity - sandbox for Moshi and Retrofit API calls

---
+++ app/src/main/java/com/example/basics/d17/D17MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.basics.d17
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.basics.R
+
+class D17MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_d17_main)
+    }
+}
