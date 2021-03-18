bd3569e7e0d1074fd5253022e6c20f7cb69d9057
Danilo <danilo.devedzic@outlook.com>
2021-03-18 14:22:49 +0100

    Using RxJava3 with Retrofit

--- app/src/main/java/com/example/basics/d17/D17MainActivity.kt
+++ app/src/main/java/com/example/basics/d17/D17MainActivity.kt
@@ -6,43 +6,48 @@ import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import com.example.basics.R
-import com.squareup.moshi.Moshi
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.disposables.Disposable
+import io.reactivex.rxjava3.schedulers.Schedulers
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
 
-class D17MainActivity : AppCompatActivity(), Callback<List<Repo>> {
+class D17MainActivity : AppCompatActivity()/*, Callback<List<Repo>>*/ {
     companion object {
         const val TAG = "D17MainActivity"
         const val GITHUB_API_URL = "https://api.github.com/"
     }
 
+    private lateinit var disposable: Disposable
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_d17_main)
 
+        val lvRepos = findViewById<ListView>(R.id.d17_lvRepos)
+
         val retrofit = with(Retrofit.Builder()) {
             baseUrl(GITHUB_API_URL)
             addConverterFactory(MoshiConverterFactory.create())
+            addCallAdapterFactory(RxJava3CallAdapterFactory.create())
             build()
         }
 
         val service = retrofit.create(GithubService::class.java)
         val repos = service.listRepos("chodomir")
-        repos.enqueue(this)
+
+        disposable = repos.subscribeOn(Schedulers.io())
+                            .observeOn(AndroidSchedulers.mainThread())
+                            .subscribe(
+                                { list -> lvRepos.adapter = ArrayAdapter<Repo>(this, R.layout.d17_simple_list_item, list) },
+                                { t -> Log.d(TAG, "Error: ${t.message}") }
+                            )
     }
 
-    override fun onFailure(call: Call<List<Repo>>, t: Throwable) {
-        Log.d(TAG, "Request failed!")
-    }
+    override fun onDestroy() {
+        super.onDestroy()
 
-    override fun onResponse(call: Call<List<Repo>>, response: Response<List<Repo>>) {
-        response.body()?.let {
-            val lvRepos = findViewById<ListView>(R.id.d17_lvRepos)
-            val adapter = ArrayAdapter<Repo>(this, R.layout.d17_simple_list_item, it)
-            lvRepos.adapter = adapter
-        } ?: Log.d(TAG, "Response empty!")
+        disposable.dispose()
     }
 }
--- app/src/main/java/com/example/basics/d17/GithubService.kt
+++ app/src/main/java/com/example/basics/d17/GithubService.kt
@@ -1,10 +1,10 @@
 package com.example.basics.d17
 
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
+import io.reactivex.rxjava3.core.Single
 
 interface GithubService {
     @GET("users/{user}/repos")
-    fun listRepos(@Path("user") user: String): Call<List<Repo>>
+    fun listRepos(@Path("user") user: String): Single<List<Repo>>
 }
